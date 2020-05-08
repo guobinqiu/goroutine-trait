33e1dc87a24b3f5f6006c70486e7e01a72c1e355
邱国宾 <qiuguobin@gracepsy.com>
2020-05-08 20:27:06 +0800

    refactoring

--- main.go
+++ main.go
@@ -31,44 +31,31 @@ import (
 //协程：非抢占式轻量级线程
 //非抢占式：非抢占式让原来正在运行的进程继续运行，直至该进程完成或发生某种事件（如I/O请求），才主动放弃处理机
 func main() {
-	//看源码太累，黑盒测试找规律吧
-	//oneCoreWithoutIO()    //从输出结果看，单核并发（concurrency），后面应该有一个队列，每个协程从队列里依次取出来执行。让原来正在运行的协程继续运行，直至该协程完成
-	//multiCoresWithoutIO() //从输出结果看，多核并行（parellel），很难看出效果
-	oneCoreWithIO()         //从输出结果看，单核并发（concurrency），后面应该有一个队列，每个协程从队列里依次取出来执行。发生了io请求，当前协程主动挂起，让其他协程运行
-	//multiCoresWithIO()    //从输出结果看，多核并行（parellel），很难看出效果
+	runtime.GOMAXPROCS(1) //确保单核容易观察
+
+	WithoutIO() //从输出结果看，后面应该有一个队列，每个协程从队列里依次取出来执行。让原来正在运行的协程继续运行，直至该协程完成
+	WithIO()    //从输出结果看，后面应该有一个队列，每个协程从队列里依次取出来执行。发生了io请求，当前协程主动挂起，让其他协程运行
+	Sched()     //runtime.Gosched打破了非抢占式特性，使当前协程被动挂起，让其他协程运行
 }
 
-//一核没io
-func oneCoreWithoutIO() {
-	runtime.GOMAXPROCS(1)
+func WithoutIO() {
 	go withoutIO("g1")
 	go withoutIO("g2")
 	go withoutIO("g3")
 	time.Sleep(5 * time.Second)
 }
 
-//多核没io
-func multiCoresWithoutIO() {
-	go withoutIO("g1")
-	go withoutIO("g2")
-	go withoutIO("g3")
-	time.Sleep(5 * time.Second)
-}
-
-//一核有io
-func oneCoreWithIO() {
-	runtime.GOMAXPROCS(1)
+func WithIO() {
 	go withIO("g1")
 	go withIO("g2")
 	go withIO("g3")
 	time.Sleep(5 * time.Second)
 }
 
-//多核有io
-func multiCoresWithIO() {
-	go withIO("g1")
-	go withIO("g2")
-	go withIO("g3")
+func Sched() {
+	go sched("g1")
+	go sched("g2")
+	go sched("g3")
 	time.Sleep(5 * time.Second)
 }
 
@@ -81,6 +68,15 @@ func withoutIO(name string) {
 func withIO(name string) {
 	for i := 0; i < 10; i++ {
 		http.Get("http://www.baidu.com")
+		fmt.Println(name, ":", i)
+	}
+}
+
+func sched(name string) {
+	for i := 0; i < 10; i++ {
+		if i == 5 {
+			runtime.Gosched()
+		}
 		fmt.Println(name, ":", i)
 	}
 }
